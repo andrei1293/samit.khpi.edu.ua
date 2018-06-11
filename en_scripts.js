3114509ef94eda91d0c48beb650e6d88ed2f7c20
Andrii Kopp <kopp93@gmail.com>
2018-06-11 15:11:31 +0300

    Language select section with flags.

--- en_scripts.js
+++ en_scripts.js
@@ -18,7 +18,9 @@ $(document).ready(function(){
 	$("#login").remove();
 
 	// add menu link programmatically
+	$("#navbar .menu").append('<li><a href="http://samit.khpi.edu.ua/issue/view/7386">Current</a></li>');
 	$("#navbar .menu").append('<li><a href="http://samit.khpi.edu.ua/pages/view/editorialBoard">Editorial board</a></li>');
+	$("#navbar .menu").append('<li><a href="http://samit.khpi.edu.ua/pages/view/ethics">Publishing ethics</a></li>');
 	$("#navbar .menu").append('<li><a href="http://samit.khpi.edu.ua/pages/view/indexing">Indexing</a></li>');
 	$("#navbar .menu").append('<li><a href="http://samit.khpi.edu.ua/about/journalSponsorship">Sponsorship</a></li>');
 	$("#navbar .menu").append('<li><a href="http://samit.khpi.edu.ua/about/history">History</a></li>');
@@ -29,5 +31,20 @@ $(document).ready(function(){
 	// editor info
 	$("#editorialTeam #editors h3").text("Website editor");
 	$("#editorialTeam #editors").before("<p>In the editorial board there is no proofreader and installer. The work on creating an original layout of the article relates to the author.</p>");
+	
+	// language select
+	document.getElementById("sidebarDevelopedBy").innerHTML += '<p><a onclick="changeLanguageFast(\'uk_UA\')"><img src="http://samit.khpi.edu.ua/manager/files/images/ua_flag.png" alt="UA"></a> ' +
+		'<a onclick="changeLanguageFast(\'en_US\')"><img src="http://samit.khpi.edu.ua/manager/files/images/en_flag.png" alt="EN"></a> ' +
+		'<a onclick="changeLanguageFast(\'ru_RU\')"><img src="http://samit.khpi.edu.ua/manager/files/images/ru_flag.png" alt="RU"></a></p>';
 });
+
+function changeLanguageFast(new_locale) {
+	var base_url = "http://samit.khpi.edu.ua/pages/view/ethics";
+	var current_url = document.URL;
+
+	var redirect_url = 'http://samit.khpi.edu.ua/user/setLocale/NEW_LOCALE?source=%2Fpages%2Fview%2Fethics';
+	redirect_url = redirect_url.replace("NEW_LOCALE", new_locale);
+
+	window.location.href = redirect_url;
+}
 // ]]></script>
--- ua_scripts.js
+++ ua_scripts.js
@@ -18,7 +18,9 @@ $(document).ready(function(){
 	$("#login").remove();
 
 	// add menu link programmatically
+	$("#navbar .menu").append('<li><a href="http://samit.khpi.edu.ua/issue/view/7386">Поточний випуск</a></li>');
 	$("#navbar .menu").append('<li><a href="http://samit.khpi.edu.ua/pages/view/editorialBoard">Редакційна колегія</a></li>');
+	$("#navbar .menu").append('<li><a href="http://samit.khpi.edu.ua/pages/view/ethics">Видавнича етика</a></li>');
 	$("#navbar .menu").append('<li><a href="http://samit.khpi.edu.ua/pages/view/indexing">Індексація</a></li>');
 	$("#navbar .menu").append('<li><a href="http://samit.khpi.edu.ua/about/journalSponsorship">Фінансування</a></li>');
 	$("#navbar .menu").append('<li><a href="http://samit.khpi.edu.ua/about/history">Історія журналу</a></li>');
@@ -26,5 +28,20 @@ $(document).ready(function(){
 	// editor info
 	$("#editorialTeam #editors h3").text("Редактор сайту");
 	$("#editorialTeam #editors").before("<p>В редакційній колегії відсутні коректор і верстальник. Робота зі створення оригінал-макету статті покладається на автора.</p>");
+	
+	// language select
+	document.getElementById("sidebarDevelopedBy").innerHTML += '<p><a onclick="changeLanguageFast(\'uk_UA\')"><img src="http://samit.khpi.edu.ua/manager/files/images/ua_flag.png" alt="UA"></a> ' +
+		'<a onclick="changeLanguageFast(\'en_US\')"><img src="http://samit.khpi.edu.ua/manager/files/images/en_flag.png" alt="EN"></a> ' +
+		'<a onclick="changeLanguageFast(\'ru_RU\')"><img src="http://samit.khpi.edu.ua/manager/files/images/ru_flag.png" alt="RU"></a></p>';
 });
+
+function changeLanguageFast(new_locale) {
+	var base_url = "http://samit.khpi.edu.ua/pages/view/ethics";
+	var current_url = document.URL;
+
+	var redirect_url = 'http://samit.khpi.edu.ua/user/setLocale/NEW_LOCALE?source=%2Fpages%2Fview%2Fethics';
+	redirect_url = redirect_url.replace("NEW_LOCALE", new_locale);
+
+	window.location.href = redirect_url;
+}
 // ]]></script>
